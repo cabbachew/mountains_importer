964f5e5afa4a691f6cf99cfaac2c50c757bc2b52
Daniel Oh <102335309+cabbachew@users.noreply.github.com>
2022-12-10 21:52:42 +0900

    use chartkick and define helper methods to graph mountain counts by difficulty

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
+  include MountainsHelper
   def home
     @mountains = Mountain.all
+    @chart_data = counts_by_difficulty(Mountain)
   end
 end
--- app/helpers/mountains_helper.rb
+++ app/helpers/mountains_helper.rb
@@ -1,2 +1,25 @@
 module MountainsHelper
+  def counts_by_difficulty(mountains)
+    groups = categorize_by_height(mountains).count
+    order_by_category(groups)
+  end
+
+  private
+  def categorize_by_height(mountains)
+    mountains.group("case 
+      when height <800 then 'low' 
+      when height >=800 and height <1100 then 'medium'
+      when height >=1100 then 'high'
+    end")
+  end
+
+  def order_by_category(groups)
+    groups.sort_by do |k,v|
+      case k
+      when "low" then 1
+      when "medium" then 2
+      when "high" then 3
+      end
+    end
+  end
 end
