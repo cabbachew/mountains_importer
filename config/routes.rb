244a5d82d1049c9b568123ac1e4831bdac6fb3aa
Daniel Oh <102335309+cabbachew@users.noreply.github.com>
2022-12-10 17:04:56 +0900

    define custom route to import mountains using the collection block

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :mountains
-  get 'pages/home'
+  resources :mountains do
+    collection do
+      post :import
+    end
+  end
+  root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
